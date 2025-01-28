c2513224333d6ee0ea64af62645d1d6f257c558a
hirokidesuga <isdhrk.program@gmail.com>
2025-01-29 00:34:47 +0900

    initialize

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# S3にアップロード
+aws s3 sync static/ s3://hirokit-prod/siteStaticContents/ --delete
+
+# CloudFrontのキャッシュを無効化
+DISTRIBUTION_ID="E3JJQAFI7J0YYX"
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" 
