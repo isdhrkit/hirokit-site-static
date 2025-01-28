b896e5b0600da8c4d824e435b0aceb43295b124c
hirokidesuga <isdhrk.program@gmail.com>
2025-01-29 05:03:59 +0900

    deploy.sh コマンド修正

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # S3にアップロード
-aws s3 sync ./ s3://hirokit-prod/siteStaticContents/ --delete
+aws s3 sync ./ s3://hirokit-prod/siteStaticContents/ --delete --exclude "deploy.sh" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store"
 
 # CloudFrontのキャッシュを無効化
 DISTRIBUTION_ID="E3JJQAFI7J0YYX"
