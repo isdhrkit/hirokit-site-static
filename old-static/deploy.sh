88c3f2b0708c7ccc0be3d5e1a258a2991198ee20
hirokidesuga <isdhrk.it@gmail.com>
2025-02-02 06:13:17 +0900

    元サイトページ削除

--- old-static/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# S3にアップロード
-aws s3 sync ./ s3://hirokit-prod/siteStaticContents/ --delete --exclude "deploy.sh" --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store"
-
-# CloudFrontのキャッシュを無効化
-DISTRIBUTION_ID="E3JJQAFI7J0YYX"
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" 
