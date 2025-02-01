bdd8f75853d94a6e9a8fe2502a7aaabf588b112e
hirokidesuga <isdhrk.it@gmail.com>
2025-02-02 06:20:27 +0900

    CloudFrontキャッシュ無効化コメントを微修正

--- next-static/deploy.sh
+++ next-static/deploy.sh
@@ -3,6 +3,6 @@
 # S3にアップロード
 aws s3 sync ./out s3://hirokit-prod/siteStaticContents/ --delete --exclude "deploy.sh"
 
-# CloudFrontのキャッシュを無効化
+# CloudFrontのキャッシュを無効
 DISTRIBUTION_ID="E3JJQAFI7J0YYX"
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" 
